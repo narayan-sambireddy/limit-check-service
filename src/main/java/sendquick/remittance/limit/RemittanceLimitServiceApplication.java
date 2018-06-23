0b2eb6356f6f0cba60888207d275860a5f68f857
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-06-24 01:03:32 +0530

    Initial commit

---
+++ src/main/java/sendquick/remittance/limit/RemittanceLimitServiceApplication.java
@@ -0,0 +1,12 @@
+package sendquick.remittance.limit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RemittanceLimitServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RemittanceLimitServiceApplication.class, args);
+	}
+}
