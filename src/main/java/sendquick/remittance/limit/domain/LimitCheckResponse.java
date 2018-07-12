530fe229620c2ebbc782029cc1f680c5ea8bb03c
Narayan Sambi Reddy <narayan-sambireddy@outlook.com>
2018-07-12 07:35:15 +0530

    Added LimitCheckResponse

---
+++ src/main/java/sendquick/remittance/limit/domain/LimitCheckResponse.java
@@ -0,0 +1,55 @@
+package sendquick.remittance.limit.domain;
+
+/**
+ *
+ * @author narayana
+ *
+ */
+public class LimitCheckResponse<T> {
+
+    T data;
+    boolean error;
+    boolean errorMsg;
+    Integer code;
+
+    private LimitCheckResponse(Integer code, T data) {
+        this.code = code;
+        this.data = data;
+    }
+
+    public static LimitCheckResponse of(Integer code, T data) {
+        return new LimitCheckResponse(code, data);
+    }
+
+    public T getData() {
+        return data;
+    }
+
+    public void setData(T data) {
+        this.data = data;
+    }
+
+    public boolean isError() {
+        return error;
+    }
+
+    public void setError(boolean error) {
+        this.error = error;
+    }
+
+    public boolean isErrorMsg() {
+        return errorMsg;
+    }
+
+    public void setErrorMsg(boolean errorMsg) {
+        this.errorMsg = errorMsg;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+}
